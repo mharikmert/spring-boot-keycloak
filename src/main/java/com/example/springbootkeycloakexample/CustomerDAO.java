cc0dc1fae666ec7a23c7c93c88e448255ad4c0a4
mharikmert <mhilmiarikmert@gmail.com>
2021-08-25 14:38:46 +0300

    Rename package

--- src/main/java/com/example/springbootkeycloakexample/CustomerDAO.java
+++
@@ -1,6 +0,0 @@
-package com.example.springbootkeycloakexample;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerDAO extends JpaRepository<Customer, Long> {
-}
